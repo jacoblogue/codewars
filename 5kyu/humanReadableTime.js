0b3d389ec85bf3be65e883b793923c46f1a1e23f
Jacob Logue <jakelogue84@gmail.com>
2022-05-21 17:18:35 -0400

    add human readable time

---
+++ 5kyu/humanReadableTime.js
@@ -0,0 +1,21 @@
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+function humanReadable(seconds) {
+  let hours = Math.floor(seconds / 3600);
+  seconds -= hours * 3600;
+  let minutes = Math.floor(seconds / 60);
+  seconds -= minutes * 60;
+
+  function formatNumber(x) {
+    return ("0" + x).slice(-2);
+  }
+
+  return `${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(
+    seconds
+  )}`;
+}
