d7342c412982e707c535cdff6efe1aecffa66fa7
Jacob Logue <jakelogue84@gmail.com>
2022-06-22 20:09:05 -0400

    passes tests

--- 7kyu/how_green_is_my_valley.py
+++ 7kyu/how_green_is_my_valley.py
@@ -56,15 +56,14 @@
 
 
 def make_valley(arr):
-    left = sorted(arr, reverse=True)
-    right = sorted(arr)
-    result = []
-    for i in range(len(arr)):
-        if left[i] >= right[i]:
 
+    res = []
+    arr.sort(reverse=True)
+    print(arr)
+    while len(arr) > 0:
+        res.insert(0, arr.pop())
+        if len(arr) == 0:
+            break
+        res.append(arr.pop())
 
-            # Tests
-print(
-    f'{make_valley([17, 17, 15, 14, 8, 7, 7, 5, 4, 4, 1])} should equal [17, 15, 8, 7, 4, 1, 4, 5, 7, 14, 17] => {make_valley([17, 17, 15, 14, 8, 7, 7, 5, 4, 4, 1]) == [17, 15, 8, 7, 4, 1, 4, 5, 7, 14, 17]} \n')
-
-print(f'{make_valley([20, 18, 16, 15, 14, 14, 13, 13, 10, 9, 4, 4, 4, 1])} should equal [20, 16, 14, 13, 10, 4, 4, 1, 4, 9, 13, 14, 15, 18] => {make_valley([20, 18, 16, 15, 14, 14, 13, 13, 10, 9, 4, 4, 4, 1]) == [20, 16, 14, 13, 10, 4, 4, 1, 4, 9, 13, 14, 15, 18] }')
+    return list(reversed(res)) if len(res) % 2 == 0 else res
