ef353e2154175171cd17017c52d2aa33965feafe
Jacob Logue <jakelogue84@gmail.com>
2022-08-07 19:13:44 -0400

    initial commit

---
+++ 8kyu/WhatIsBetween.cs
@@ -0,0 +1,17 @@
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+using System.Linq;
+
+public class Kata
+{
+  public static int[] Between(int a, int b) 
+  {
+    return Enumerable.Range(a, b - a + 1).ToArray();
+  }
+}
