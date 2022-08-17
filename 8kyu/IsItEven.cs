c50bad7ff2301a98375a3e31aed029fa692d573a
Jacob Logue <jakelogue84@gmail.com>
2022-08-17 11:22:54 -0400

    initial commit

---
+++ 8kyu/IsItEven.cs
@@ -0,0 +1,14 @@
+// In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even (or not).
+
+// The function needs to return either a true or false.
+
+// Numbers may be positive or negative, integers or floats.
+
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+public class Number
+{
+  public bool IsEven(double n) => n % 2 == 0;
+}
