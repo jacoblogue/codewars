1c20ca8d7c715ab438d1df448ef8bc7fca9993c2
Jacob Logue <jakelogue84@gmail.com>
2022-08-27 12:19:58 -0400

    reverse modulo operation

--- 8kyu/ILoveYouALittleALotPassionatelyNotAtAll.cs
+++ 8kyu/ILoveYouALittleALotPassionatelyNotAtAll.cs
@@ -5,6 +5,6 @@ public class Kata
   public static string HowMuchILoveYou(int nb_petals)
   {
     string[] phrases = { "I love you", "a little", "a lot", "passionately", "madly", "not at all" };
-    return (nb_petals > phrases.Length) ? phrases[phrases.Length % (nb_petals - 1)] : phrases[nb_petals - 1];
+    return (nb_petals > phrases.Length) ? phrases[(nb_petals - 1) % phrases.Length] : phrases[nb_petals - 1];
   }
 }
